95293330fc1decd238f23ec0f648a94f5cd02eeb
KennethClark2 <clark_kenneth2024@gvr5.net>
2021-03-16 18:37:28 +0000

    Initial commit

---
+++ main.sh
@@ -0,0 +1,10 @@
+chromium-browser --app --no-sandbox --no-first-run --disable-dev-shm-usage --disable-background-networking
+
+# --app
+# --no-wifi
+# --no-sandbox
+# --as-process
+# --no-first-run
+# --disable-gpu-sandbox
+# --disable-dev-shm-usage
+# --disable-background-networking
